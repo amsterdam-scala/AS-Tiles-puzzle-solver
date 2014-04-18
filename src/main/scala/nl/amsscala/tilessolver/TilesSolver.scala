2289f083fe8ee150c223248a862ead12a193288a
F B <vdenberg+github@gmail.com>
2014-04-18 20:45:51 +0200

    Some clean-up

--- src/main/scala/nl/amsscala/tilessolver/TilesSolver.scala
+++ src/main/scala/nl/amsscala/tilessolver/TilesSolver.scala
@@ -1,22 +1,21 @@
 package nl.amsscala
-package nl.amsscala.tilessolver
+package tilessolver
+
+import scala.annotation.tailrec
 
 object TilesSolver {
 
-  object Direction extends Enumeration {
-    case class Direction extends Val {
-      def =!=(other: Direction) = {
-        this match {
-          case O => false
-          case N => other == S
-          case S => other == N
-          case E => other == W
-          case W => other == E
-        }
+  case class Direction {
+    def =!=(other: Direction) =
+      this match {
+        case C => false
+        case N => other == S
+        case S => other == N
+        case E => other == W
+        case W => other == E
       }
-    }
-    val O, N, E, S, W = Direction()
   }
+  val C, N, E, S, W = Direction()
 
   import Direction._
 
@@ -25,24 +24,25 @@ object TilesSolver {
   type Path = List[Tile]
 
   implicit class PathWrapper(path: Path) {
-    def isPrepandable(tile: Tile) = {
-      if (path.isEmpty) tile.end == O
-      else path.head.start =!= tile.end
+    def isPrepandable(tile: Tile): Boolean = {
+      if (path.isEmpty) tile.end == C else path.head.start =!= tile.end
     }
   }
 
+  @tailrec
   def solver(source: List[Tile], path: Path): List[Path] = {
     val flatmapped = source.zipWithIndex.filter(ti => path.isPrepandable(ti._1)).flatMap(ti => {
       var res = solver(source.take(ti._2) ++ source.drop(ti._2 + 1), ti._1 +: path)
-      if (ti._1.start == O) res = res :+ (ti._1 +: path)
+      if (ti._1.start == C) res = res :+ (ti._1 +: path)
       res
     })
     flatmapped
   }
-  val l = List(Tile(O, E), Tile(W, O))
+
+  val l = List(Tile(C, E), Tile(W, C))
   solver(l, List())
 
-  val l2 = List(Tile(S, E), Tile(W, E), Tile(N, O), Tile(O, E), Tile(W, S), Tile(O, E), Tile(S, W), Tile(N, E), Tile(N, S), Tile(W, O))
+  val l2 = List(Tile(S, E), Tile(W, E), Tile(N, C), Tile(C, E), Tile(W, S), Tile(C, E), Tile(S, W), Tile(N, E), Tile(N, S), Tile(W, C))
 
   solver(l2, List()).groupBy(_.length).maxBy(_._1)._2.mkString("\n")
 
