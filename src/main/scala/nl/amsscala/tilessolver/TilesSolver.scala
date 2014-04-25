b6ecaeb692896dbcfe8baaca9212ecee270c2fe4
Code-IT-Up <vdenberg+github@gmail.com>
2014-04-25 09:40:16 +0200

    Tidyed up

--- src/main/scala/nl/amsscala/tilessolver/TilesSolver.scala
+++ src/main/scala/nl/amsscala/tilessolver/TilesSolver.scala
@@ -3,6 +3,9 @@ package tilessolver
 
 import scala.annotation.tailrec
 
+/** @author A'dam Scala Tiles-puzzle-solver team
+ */
+
 object TilesSolver extends App {
 
   /** Enumeration of the connection side of a tile*/
@@ -44,53 +47,47 @@ object TilesSolver extends App {
     Tile(S, E), Tile(W, E), Tile(N, C), Tile(C, E), Tile(E, S),
     Tile(C, E), Tile(S, W), Tile(N, E), Tile(N, S), Tile(W, C))
 
-  /** @param	trail	Comparative objects A
-   *  @param	combinator	comparative objects B
-   *  @param	source	Available tiles to combine with
-   *  @param	onHand	Actual tiles left over
-   *  @param	explored	Actual combination
-   *  @param	accu	List of paths discovered
-   */
-  def walk(
-    trail: List[Tile],
-    combinator: List[Tile],
-    source: List[Tile],
-    onHand: List[Tile],
-    explored: Path,
-    accu: List[Path]): List[Path] = {
-    if (trail.isEmpty) (explored +: accu)//.distinct // distinct is necessary, don't know why
-    else if (combinator.isEmpty) { // Try a new walk 
-      walk(trail.tail,
-        source,
-        source,
-        onHand = source,
-        Nil,
-        if (explored.isEmpty) accu else explored +: accu)
-    } else
-      walk(trail, combinator.tail, source, onHand, explored,
-        (if (trail.head.start isJoinable combinator.head.end) {
-          walk(
-            trail = List(combinator.head), // explore further with new found tile
-            // Invoke a found sequence by empty list
-            combinator = if (combinator.head.start == C) Nil else onHand diff List(combinator.head),
-            source = source,
-            onHand = onHand diff Vector(combinator.head), // explore further without used tile
-            explored = (if (combinator.head.start == C) List(combinator.head, trail.head)
-            else List(trail.head)) ++ explored,
-            accu = accu)
-        } else List.empty) ++ accu).distinct
-  }
-
   def solve(tiles: List[Tile]) = {
+    /** Available tiles to combine with*/
     val tilesNotEndingInTheMiddle = tiles.filter(_.end != C)
+
+    def walk(trail: List[Tile], //Comparative objects A
+             combinator: List[Tile], //comparative objects B
+             onHand: List[Tile], //Actual tiles left over
+             explored: Path, //Actual promising combination in progress
+             accu: List[Path] //List of paths discovered
+             ): List[Path] = {
+      if (trail.isEmpty) (explored +: accu).distinct // distinct is necessary, don't know why
+      else if (combinator.isEmpty) { // Try a new walk 
+        walk(trail.tail,
+          tilesNotEndingInTheMiddle,
+          onHand = tilesNotEndingInTheMiddle,
+          Nil,
+          if (explored.isEmpty || (explored.head.start != C)) accu else explored +: accu)
+      } else
+        walk(trail, combinator.tail, onHand, explored,
+          (if (trail.head.start isJoinable combinator.head.end) {
+            walk(
+              trail = List(combinator.head), // explore further with new found tile
+              // Invoke a complete found ending sequence by empty list
+              combinator = if (combinator.head.start == C) Nil else onHand diff List(combinator.head),
+              onHand = onHand diff Vector(combinator.head), // explore further without used tile
+              explored =
+                (if (combinator.head.start == C) List(combinator.head, trail.head)
+                else List(trail.head)) ++ explored,
+              accu = accu)
+          } else Nil) ++ accu)
+    } // def walk(
+
     walk(trail = tiles.filter(_.end == C).distinct, // Start with ending tiles, one of each
-      combinator = tilesNotEndingInTheMiddle.distinct, // Other tile to compare with, one of each
-      source = tilesNotEndingInTheMiddle, // available tiles to compose with
+      combinator = tilesNotEndingInTheMiddle.distinct, // Other tiles to compare with, one of each
       onHand = tilesNotEndingInTheMiddle, // Nearly all tiles to start over
       explored = Nil, // intermediate result
       accu = Nil)
-  }
+  } // def solve(
 
-  println(solve(l2).distinct.mkString("\n")) // Accumulated results
+  val result = solve(l2)
 
+  println("Diff " + (result diff result.distinct))
+  println(result.mkString("\n")) // Accumulated results
 }
