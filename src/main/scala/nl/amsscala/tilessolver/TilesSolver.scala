ad59900dc64bb88e6a489d49bd563d883d12de41
Code-IT-Up <vdenberg+github@gmail.com>
2014-04-29 23:05:50 +0200

    Ready and tested

--- src/main/scala/nl/amsscala/tilessolver/TilesSolver.scala
+++ src/main/scala/nl/amsscala/tilessolver/TilesSolver.scala
@@ -5,6 +5,8 @@ import scala.annotation.tailrec
 
 /** Tile solver program
  *
+ *  Find the longest chain of connected tiles.
+ *
  *  Signature is findPaths(tiles: TilesToUse): Set[Path]
  *  where TilesToUse is an unordered List[Tile]
  *  and   Path is an ordered List[Tile]
@@ -50,67 +52,61 @@ object TilesSolver extends App {
   /** Descriptor for a tile direction indicated with a arrow
    *  @param	start The from or incoming of tile (tail of arrow)
    *  @param	end	The to or outgoing side of tile (arrowhead)
-   *  @throws
+   *  @throws	java.lang.IllegalArgumentException
    */
   case class Tile(val start: Direction, val end: Direction) {
     require(start != end, s"Not a proper tile definition, given $start, $end are the same.")
   }
 
-  type Path = List[Tile]
-  type TilesToUse = Path
-
-  val l = List(Tile(C, E), Tile(W, C))
+  type Chain = List[Tile]
+  type TilesToUse = Chain
 
   /** Returns a set of possible paths starting
    *  and ending with a start and ending tile.
    */
-  def findPaths(tiles: TilesToUse): Set[Path] = {
+  def findChains(tiles: TilesToUse): Set[Chain] = {
     /** Available tiles to combine with. */
     val tilesNotEndingInTheMiddle = tiles.filter(_.end != C)
 
     case class AssetHandling(val candidates: TilesToUse = tilesNotEndingInTheMiddle, //comparative objects B
                              onHand: TilesToUse = tilesNotEndingInTheMiddle, //Actual unused tiles
-                             outHand: Path = Nil //Actual promising combinations in progress
+                             outHand: Chain = Nil //Actual promising combinations in progress
                              ) {
 
-      def completeFoundEndingSequence(previousTile: Tile) =
+      def processFoundTile(previousTile: Tile) =
         {
-          // Test if we found a path with a center ending tile
+          val restOnHand = onHand diff List(candidates.head)
+          val (assetToTransfer, influencePath) = // Test if we found a path with a center ending tile
+            // Invoke a complete found ending sequence by empty list
+            if (candidates.head.start == C) (List(candidates.head, previousTile), Nil)
+            else (List(previousTile), restOnHand)
 
-          val rest = if (candidates.head.start == C) Nil else onHand diff List(candidates.head)
-
-          val assetToTransfer =
-            if (candidates.head.start == C) List(candidates.head, previousTile) else List(previousTile)
-
-          // Invoke a complete found ending sequence by empty list
-          AssetHandling(if (candidates.head.start == C) Nil else onHand diff List(candidates.head),
-            onHand diff List(candidates.head), // explore further without used tile
+          AssetHandling(influencePath,
+            restOnHand, // explore further without used tile
             // If ending tile save 2 tiles, including the ending one
             assetToTransfer ++ outHand)
         }
 
+      def isNotValidTileChain = outHand.isEmpty || (outHand.head.start != C)
     }
 
     def walk(trail: TilesToUse, //Comparative objects A
              asset: AssetHandling,
-             maintainedPaths: Set[Path] /*List of paths already discovered*/ ): Set[Path] = {
+             maintainedPaths: Set[Chain] /*List of paths already discovered*/ ): Set[Chain] = {
+
       if (trail.isEmpty) maintainedPaths + asset.outHand // distinct of a set is necessary, don't know why
       else if (asset.candidates.isEmpty) { // Try a new walk 
-        walk(trail.tail, AssetHandling(),
-          if (asset.outHand.isEmpty || (asset.outHand.head.start != C)) maintainedPaths
-          else maintainedPaths + asset.outHand)
+        walk(trail.tail,
+          AssetHandling( /*onHand = asset.onHand*/ ),
+          if (asset.isNotValidTileChain) maintainedPaths else maintainedPaths + asset.outHand)
       } else // Do a matching with each other tile
         walk(trail, AssetHandling(asset.candidates.tail, asset.onHand, asset.outHand),
-
-          maintainedPaths ++
-            (if (trail.head.start isJoinable asset.candidates.head.end) {
-              walk( // Start of actual walk parameter list
-                List(asset.candidates.head), // explore further with new found tile
-
-                asset.completeFoundEndingSequence(trail.head),
-
-                maintainedPaths) // End of actual walk parameter list
-            } else Nil))
+          maintainedPaths ++ (if (trail.head.start isJoinable asset.candidates.head.end) {
+            walk( // Start of actual walk parameter list
+              List(asset.candidates.head), // explore further with new found tile
+              asset.processFoundTile(trail.head),
+              maintainedPaths) // End of actual walk parameter list
+          } else Nil))
     } // def walk(
 
     walk(trail = tiles.filter(_.end == C).distinct, // Start with ending tiles, one of each
@@ -118,7 +114,7 @@ object TilesSolver extends App {
       maintainedPaths = Set.empty)
   } // def findPaths(
 
-  val result = findPaths(List( // The example on the photo
+  val result = findChains(List( // The example on the photo
     Tile(S, E), Tile(W, E), Tile(N, C), Tile(C, E), Tile(W, S),
     Tile(C, E), Tile(S, W), Tile(N, E), Tile(N, S), Tile(W, C)))
 
--- src/test/scala/nl/amsscala/tilessolver/TilesSolverSuite.scala
+++ src/test/scala/nl/amsscala/tilessolver/TilesSolverSuite.scala
@@ -18,14 +18,14 @@ class TilesSolverSuite extends FunSpec with GivenWhenThen {
     List(Tile(W, E), Tile(W, E)),
     List(Tile(W, E), Tile(E, W)),
     List(Tile(C, E), Tile(E, C)),
-    List(Tile(C, E), Tile(C, E), Tile(E, C)),
+    List(Tile(C, E), Tile(C, E), Tile(E, C)), // 7
     List(Tile(C, E), Tile(E, C), Tile(C, E)),
-    List(Tile(N, C), Tile(C, S)),
+    List(Tile(N, C), Tile(C, S)), // 9
     List(Tile(C, S), Tile(N, C)),
-    List(Tile(C, S), Tile(C, S), Tile(N, C)),
+    List(Tile(C, S), Tile(C, S), Tile(N, C)), // 11
     List(Tile(C, S), Tile(N, C), Tile(C, S)),
     List(Tile(N, C), Tile(C, S), Tile(C, S)),
-    List(Tile(N, C), Tile(N, C), Tile(C, S), Tile(C, S)),
+    List(Tile(N, C), Tile(N, C), Tile(C, S), Tile(C, S)), //14
     List(Tile(C, S), Tile(N, C), Tile(N, C), Tile(C, S)),
     List(Tile(C, S), Tile(W, C), Tile(N, C), Tile(C, E)),
     List(Tile(S, E), Tile(W, E), Tile(N, C), Tile(C, E), Tile(E, S), // Modified example
@@ -38,7 +38,7 @@ class TilesSolverSuite extends FunSpec with GivenWhenThen {
   describe("A solution of the Tile problem") {
 
     it("should reject an invalid tile definition") {
-      val thrown = intercept[Exception] {
+      val thrown = intercept[java.lang.IllegalArgumentException] {
         Tile(C, C)
       }
       assert(thrown.getMessage === "requirement failed: Not a proper tile definition, given C, C are the same.")
@@ -46,72 +46,63 @@ class TilesSolverSuite extends FunSpec with GivenWhenThen {
 
     it("should result in a set with one empty path") {
       given("an empty list")
-      expectResult(Set(Nil)) { TilesSolver.findPaths(cases(0)) }
+      expectResult(Set(Nil)) { TilesSolver.findChains(cases(0)) }
 
       given("single tile West Center")
-      expectResult(Set(Nil))(TilesSolver.findPaths(cases(1)))
+      expectResult(Set(Nil))(TilesSolver.findChains(cases(1)))
 
       given("single tile Center West")
-      expectResult(Set(Nil))(TilesSolver.findPaths(cases(2)))
+      expectResult(Set(Nil))(TilesSolver.findChains(cases(2)))
 
       given("single tile West East")
-      expectResult(Set(Nil))(TilesSolver.findPaths(cases(3)))
+      expectResult(Set(Nil))(TilesSolver.findChains(cases(3)))
 
       given("two same tiles")
-      expectResult(Set(Nil))(TilesSolver.findPaths(cases(4)))
+      expectResult(Set(Nil))(TilesSolver.findChains(cases(4)))
 
       given("two mirrored tiles")
-      expectResult(Set(Nil))(TilesSolver.findPaths(cases(5)))
+      expectResult(Set(Nil))(TilesSolver.findChains(cases(5)))
 
       given("two mirrored centered tiles")
-      expectResult(Set(Nil))(TilesSolver.findPaths(cases(6)))
+      expectResult(Set(Nil))(TilesSolver.findChains(cases(6)))
 
       given("three mirrored centered tiles")
-      expectResult(Set(Nil))(TilesSolver.findPaths(cases(7)))
-
-      given("three mirrored centered tiles")
-      expectResult(Set(Nil))(TilesSolver.findPaths(cases(8)))
+      cases(7).permutations.foreach(casus => assert(TilesSolver.findChains(casus) === Set(Nil)))
     }
 
-    it("given two correct centered tiles should result in a list of paths") {
-      assert(TilesSolver.findPaths(cases(9)) === Set(List(), List(Tile(C, S), Tile(N, C))))
+    it("given two correct centered tiles should result in a set of chains") {
+      cases(9).permutations.
+        foreach(casus => assert(TilesSolver.findChains(casus) === Set(List(), List(Tile(C, S), Tile(N, C)))))
     }
-    it("given two correct reversed centered tiles should result in a list of paths") {
-      assert(TilesSolver.findPaths(cases(10)) === Set(List(), List(Tile(C, S), Tile(N, C))))
-    }
-    it("given three correct reversed centered tiles should result in a list of paths") {
-      assert(TilesSolver.findPaths(cases(11)) === Set(List(), List(Tile(C, S), Tile(N, C))))
-    }
-    it("given three correct reversed centered tiles should still result in a list of paths") {
-      assert(TilesSolver.findPaths(cases(12)) === Set(List(), List(Tile(C, S), Tile(N, C))))
-    }
-    it("given 3 correct reversed centered tiles should still result in a list of paths") {
-      assert(TilesSolver.findPaths(cases(13)) === Set(List(), List(Tile(C, S), Tile(N, C))))
+
+    it("given three correct reversed centered tiles should result in a set of chains") {
+      cases(11).permutations.
+        foreach(casus => assert(TilesSolver.findChains(casus) === Set(List(), List(Tile(C, S), Tile(N, C)))))
     }
 
     // 
-    it("given four alternated reversed centered tiles should result in a list of paths") {
-      assert(TilesSolver.findPaths(cases(14)) === Set(List(), List(Tile(C, S), Tile(N, C))))
+    it("given four alternated reversed centered tiles should result in a set of chains") {
+      assert(TilesSolver.findChains(cases(14)) === Set(List(), List(Tile(C, S), Tile(N, C))))
     }
-    it("given four alternated reversed centered tiles should still result in a list of paths") {
-      assert(TilesSolver.findPaths(cases(15)) === Set(List(), List(Tile(C, S), Tile(N, C))))
+    it("given four alternated reversed centered tiles should still result in a set of chains") {
+      assert(TilesSolver.findChains(cases(15)) === Set(List(), List(Tile(C, S), Tile(N, C))))
     }
 
-    it("given four cross reversed centered tiles should still result in a list of paths") {
-      assert(TilesSolver.findPaths(cases(16)) ===
+    it("given four cross reversed centered tiles should still result in a set of chains") {
+      assert(TilesSolver.findChains(cases(16)) ===
         Set(List(), List(Tile(C, S), Tile(N, C)), List(Tile(C, E), Tile(W, C))))
     }
 
-    it("given the modified example of the site should still result in a list of paths") {
-      assert(TilesSolver.findPaths(cases(17)) ===
+    it("given the modified example of the site should still result in a set of chains") {
+      assert(TilesSolver.findChains(cases(17)) ===
         Set( //
           List(), //
           List(Tile(C, E), Tile(W, C)), //
           List(Tile(C, E), Tile(W, E), Tile(W, C))))
     }
 
-    it("given the example of the site should still result in a list of paths") {
-      assert(TilesSolver.findPaths(cases(18)) ===
+    it("given the example of the site should still result in a set of chains") {
+      assert(TilesSolver.findChains(cases(18)) ===
         Set( //
           List(), //
           List(Tile(C, E), Tile(W, S), Tile(N, S), Tile(N, E), Tile(W, C)), //
@@ -132,15 +123,14 @@ class TilesSolverSuite extends FunSpec with GivenWhenThen {
 
     it("should every time the same lists of paths, thus be stable") {
       Given("the modified example of the site all permutations (1.814.400)")
-      cases(17).permutations.foreach(casus => assert(
-        TilesSolver.findPaths(casus) ===
-          Set(List(),
-            List(Tile(C, E), Tile(W, E), Tile(W, C)),
-            List(Tile(C, E), Tile(W, C)))))
+      cases(17).permutations.foreach(casus => assert(TilesSolver.findChains(casus) ===
+        Set(List(),
+          List(Tile(C, E), Tile(W, E), Tile(W, C)),
+          List(Tile(C, E), Tile(W, C)))))
     }
 
     /* for (casus <- cases(18).permutations) assert(
-    TilesSolver.findPaths(casus) ===
+    TilesSolver.findChains(casus) ===
       Set(List(Tile(C, E), Tile(W, E), Tile(W, S), Tile(N, C)),
         List(Tile(C, E), Tile(W, S), Tile(N, C)),
         List(Tile(C, E), Tile(W, S), Tile(N, E), Tile(W, C)),
