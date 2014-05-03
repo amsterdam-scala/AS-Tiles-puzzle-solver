a3ec872089cebb3a05060933134ad7a4b993b8a0
Code-IT-Up <vdenberg+github@gmail.com>
2014-05-03 22:28:07 +0200

    Initial Gui set-up

---
+++ src/main/scala/nl/amsscala/tilessolver/TwoDim.scala
@@ -0,0 +1,31 @@
+package nl.amsscala
+package tilessolver
+
+object TwoDim extends App {
+  /** Convert List[Tile] to Map[coordinate,Tile]
+   */
+
+  import Direction._
+
+  def step(stap: Direction, ori: (Int, Int)) = stap match {
+    case N => (ori._1, ori._2 + 1)
+    case E => (ori._1 + 1, ori._2)
+    case S => (ori._1, ori._2 - 1)
+    case W => (ori._1 - 1, ori._2)
+    case _ => (ori._1, ori._2)
+  }
+
+  def listWithSum(numbers: List[Int]) = numbers.foldLeft((List[Int](), 0)) {
+    (resultingTuple, currentCoord) =>
+      (currentCoord :: resultingTuple._1, currentCoord + resultingTuple._2)
+  }
+
+  def toDim(chain: Chain) =
+    chain.foldLeft(List[((Int, Int), Tile)](), (0, 0)) {
+      (resultingTuple, currentTile) =>
+        (((resultingTuple._2, currentTile)) :: resultingTuple._1, step(currentTile.end, resultingTuple._2))
+    }._1.toMap
+
+  println(toDim(List(Tile(C, E), Tile(W, S), Tile(N, S), Tile(N, E), Tile(W, E), Tile(W, C))))
+
+}
